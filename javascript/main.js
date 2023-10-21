a2179ff526a306b3a2a6048ebd9890ecd44e109a
swetha.gopinath@texple.com <swethagnthn@gmail.com>
2023-10-21 14:44:59 +0530

    added feauture and gallery and deal and newsletter

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,57 @@
 $(document).ready(function(){
+
+    $(window).on('scroll load', function() {
+        $('#menu').removeClass('fa-times')
+        $('.navbar').removeClass('nav-toggle')
+
+        if($(window).scrollTop() > 68){
+            $('header .header-2').addClass('header-active')
+        }else{
+            $('header .header-2').removeClass('header-active')
+        }
+
+        $('section').each(function(){
+            let height = $(this).height()
+            let offset =  $(this).offset().top - 200
+            let top = $(window).scrollTop()
+            let id = $(this).attr('id')
+
+            if( top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active')
+                $('.navbar').find(`[href='#${id}]`).addClass('active')
+            }
+        })
+    })
+
+    $('.small-image img').click(function(){
+        $(this).addClass('image-active').siblings('').removeClass('image-active')
+        let image = $(this).attr('src')
+        $(this).parent().siblings('.big-image').children().attr('src', image)
+    })
     
     $('#menu').click(function(){
         $(this).toggleClass('fa-times')
         $('.navbar').toggleClass('nav-toggle')
     })
 
-    $('.owl-carousel').owlCarousel({
-        items: 1,
+    $('.home-slider').owlCarousel({
+        item: 1,
+        loop:false,
         nav: true,
-        dots: false,
-        autoplay: true,
+        dots: true,
         autoplayTimeout: 8000,
-        loop: true
+    })
+
+    $('.gallery .btn').click(function(){
+        let filter = $(this).attr('data-filter')
+        if(filter == 'all'){
+            $('.gallery .box').show(400)
+        }else{
+            $('.gallery .box').not('.'+filter).hide(200)
+            $('.gallery .box').filter('.'+filter).show(400)
+        }
+
+        $(this).addClass('button-active').siblings().removeClass('button-active')
     })
 
 })
