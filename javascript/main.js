e8fafa1cd077c0e121317d4800405554b6d9f18d
swetha.gopinath@texple.com <swethagnthn@gmail.com>
2023-10-31 12:18:51 +0530

    added carousel

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-
+    var sync1 = $("#sync1");
+   
+    sync1.owlCarousel({
+        items: 1,
+        slideSpeed: 2000,
+        nav: true,
+        autoplay: false, 
+        dots: false,
+        loop: true,
+        responsiveRefreshRate: 200,
+        navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+    }).on('changed.owl.carousel');
+    
     $(window).on('scroll load', function() {
         $('#menu').removeClass('fa-times')
         $('.navbar').removeClass('nav-toggle')
